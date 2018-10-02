56c4a17a09a5855918a28fd4c1ca044a4897fdce
Tim Biles <timbilestimbiles@gmail.com>
2018-10-02 11:52:14 -0500

    reversed arr

--- script.js
+++ script.js
@@ -1,16 +1,73 @@
-///// Problem 1 /////
+///// Problem 6 /////
 
-// Calculate the product of all elements in an array.
+// Given an array, return the reversed version of the array (a different kind of reverse though), you reverse portions of the array, you'll be given a length argument which represents the length of each portion you are to reverse.
 
-//If the array is null or is empty, the function should return null.
+// E.g
 
-const product = val => {
-  if(val === null || val.length === 0) return null;
-  return val.reduce((acc, e)=> acc*e, 1)
-}
+// selReverse([1,2,3,4,5,6], 2)
+// //=> [2,1, 4,3, 6,5]
+// if after reversing some portions of the array and the length of the remaining portion in the array is not up to the length argument, just reverse them.
 
-const num = [1, 2, 3, 4, 5]
-console.log('product >>>', product(num))
+// selReverse([2,4,6,8,10,12,14,16], 3)
+// //=> [6,4,2, 12,10,8, 16,14]
+// selReverse(array, length)
+
+const reverseNum = (arr, num) => { 
+    if(arr.length === 0){
+      return arr
+    }
+  
+    let newArr = [];
+  
+    for(let i=0; i<(num < arr.length ? num : arr.length); i++){
+      newArr.push(arr[i])
+    }
+  arr.splice(newArr, num)
+  newArr.reverse()
+  return newArr.concat(reverseNum(arr,num)) 
+  }
+  
+  console.log('reversed >>>', reverseNum([1,2,3,4,5,6], 2))
+
+
+///// Problem 5 /////
+
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+
+const spinWords = str =>{
+    return str.split(' ').map(e=>{
+      return e.length >= 5 ? e.split('').reverse().join('') : e
+    }).join(' ')
+  }
+
+///// Problem 4 /////
+
+
+// Write a class function named repeat() that takes two arguments (a string and a long integer), and returns a new string where the input string is repeated that many times. 
+
+const repeater = (str, n) =>str.repeat(n)
+
+console.log('Pete and Repeat >>>', repeater('b', 4))
+
+
+///// Problem 3 /////
+  
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+const switcheroo = x => {
+    let split = x.split('')
+    for(let i=0; i<split.length; i++){
+      if(split[i] === 'a'){
+        split[i] = 'b'
+      } else if(split[i] === 'b'){
+        split[i] = 'a'
+      }
+    }
+    return split.join('')
+  }
+  
+  console.log('switch >>>',switcheroo('abc'))
 
 
 ///// Problem 2 /////
@@ -29,3 +86,17 @@ const pillars = (p, dist, width) =>{
   
   console.log('pillars >>>', pillars(2, 20, 25))
 
+
+///// Problem 1 /////
+
+// Calculate the product of all elements in an array.
+
+//If the array is null or is empty, the function should return null.
+
+const product = val => {
+    if(val === null || val.length === 0) return null;
+    return val.reduce((acc, e)=> acc*e, 1)
+  }
+  
+  const num = [1, 2, 3, 4, 5]
+  console.log('product >>>', product(num))
