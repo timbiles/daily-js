8d925c7958c4f8beb2a1a0a59cc9295446c59523
Tim Biles <timbilestimbiles@gmail.com>
2018-10-11 12:07:50 -0500

    10-11 update

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+///// Problem 9 /////
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
+
+const time = num => {
+    let hh = Math.floor(num/3600)
+    let mm = Math.floor((num - (hh * 3600))/60)
+    let ss = Math.floor(num - (hh*3600) - (mm*60))
+  
+    if(ss < 10) ss = '0' + ss
+    if(mm < 10) mm = '0' + mm
+    if(hh < 10) hh = '0' + hh
+    return `${hh}:${mm}:${ss}`
+  }
+  
+  console.log('Time >>>>', time(6500))
+  
+
+
 ///// Problem 8 /////
 // Write a function that, given a string, returns the number of vowels in that string.
 
